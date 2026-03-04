ef611a849583097121bbab41e74b0f101b10b72d
slowwshy <arthurtoon13@gmail.com>
2026-03-04 15:37:23 -0300

    Corrigindo e adicionando

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-var express = require('express');
+const express = require('express');
 const app = express();
 const port = 4141;
-  const curriculo = {
+  const resume = {
         name: "Arthur Soares de Melo",
-        age: "18 years",
-        ability: "Java Script/Node.js, Git/GitHub, MySQL, estrutura de dados, eficiente e organizado",
-        experience: "Técnico em programção de jogos digitais"
+        age: "18 years/18 anos",
+        ability: "Java Script/Node.js, Git/GitHub, MySQL, estrutura de dados/data structure, eficiente e organizado/efficient and organized",
+        experience: "digital game programming technician/Técnico em programção de jogos digitais",
+        linksG: "https://github.com/slowwshy",
+        Email: "mail.google.com/arthurtoon13@gmail.com"
                     };
 app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-        res.render('htss', {curriculo})
+        res.render('htss', {resume})
 })
 
 
