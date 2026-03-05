4da517646d7bd03a047aae2e66f4f75fc9375314
slowwshy <arthurtoon13@gmail.com>
2026-03-05 16:32:09 -0300

    correções

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 const port = 4141;
   const resume = {
         name: "Arthur Soares de Melo",
         age: "18 years/18 anos",
-        ability: "Java Script/Node.js, Git/GitHub, MySQL, estrutura de dados/data structure, eficiente e organizado/efficient and organized",
+        ability: "Java Script/Node.js, Git/GitHub, MySQL ",
+        ability2: "estrutura de dados/data structure",
+        ability3: "eficiente e organizado/efficient and organized",
         experience: "digital game programming technician/Técnico em programção de jogos digitais",
         linksG: "https://github.com/slowwshy",
         Email: "mail.google.com/arthurtoon13@gmail.com"
                     };
+
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
 app.get('/', function(req, res){
-        res.render('htss', {resume})
+        res.render('index', {resume})
 })
 
 
---
+++ public/script.js
@@ -0,0 +1,3 @@
+function Hi(){
+    alert("Hi");
+}
