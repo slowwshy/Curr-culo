a75a69ac7a88368ca2945fbe3988eca7a4df0aa4
slowwshy <arthurtoon13@gmail.com>
2026-03-05 18:47:07 -0300

    mais correções

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const port = 4141;
   const resume = {
         name: "Arthur Soares de Melo",
         age: "18 years/18 anos",
+        local: "Brazil/Brasil, Brasília",
         ability: "Java Script/Node.js, Git/GitHub, MySQL ",
         ability2: "estrutura de dados/data structure",
         ability3: "eficiente e organizado/efficient and organized",
         experience: "digital game programming technician/Técnico em programção de jogos digitais",
         linksG: "https://github.com/slowwshy",
-        Email: "mail.google.com/arthurtoon13@gmail.com"
+        Email: "mail.google.com/arthurtoon13@gmail.com",
+        language: "english, portuguese and german"
                     };
 
 app.set('view engine', 'ejs');
